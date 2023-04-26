b1b19d631d5a36ca5a5f5eda3d7b9ebb5cd3c0f2
Hamlet2016 <martinezhamlet@gmail.com>
2023-04-26 15:03:06 -0600

    assignment 3 game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is your magic number? ");
+        string guessing = Console.ReadLine();
+        int guessing1 = int.Parse(guessing);
+        Console.WriteLine($"Your magic number is {guessing1} ");
+
+        int guessing_client;
+
+        do
+        {
+         Console.WriteLine("What is your guess? ");
+         string guessing2 = Console.ReadLine();
+         guessing_client = int.Parse(guessing2);
+
+        
+
+         if (guessing_client > guessing1)
+         {
+             Console.WriteLine("Lower");
+         }
+         else if (guessing_client < guessing1)
+         {
+            Console.WriteLine("Higher"); 
+         }
+         else
+         {
+           Console.WriteLine("You guessed it!");   
+         }
+        } while (guessing_client != guessing1);
+
+
     }
 }
